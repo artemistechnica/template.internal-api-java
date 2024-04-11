9894309b6ac843435edf49d3e0e0a3992a7e9057
nickchampagne <nick.champagne@protonmail.com>
2024-04-11 06:54:43 -0400

    Template project initial commit.

---
+++ src/main/java/com/artemistechnica/templates/internalapijava/TemplateInternalApiJavaApplication.java
@@ -0,0 +1,13 @@
+package com.artemistechnica.templates.internalapijava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateInternalApiJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateInternalApiJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/artemistechnica/templates/internalapijava/TemplateInternalApiJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artemistechnica.templates.internalapijava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateInternalApiJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

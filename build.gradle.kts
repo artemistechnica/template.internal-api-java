af1680357dc0ebb935abb8c1c372d31131120ee1
nickchampagne <nick.champagne@protonmail.com>
2024-06-09 01:08:32 -0400

    Added first pass at a make shift map/reduce

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,7 @@ dependencies {
 	implementation("io.github.git-commit-id:git-commit-id-maven-plugin:8.0.2")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-web:3.2.5")
-	implementation("com.artemistechnica.commons:commons-java:0.0.5-SNAPSHOT")
+	implementation("com.artemistechnica.commons:commons-java:0.0.6-SNAPSHOT")
 	implementation("io.micrometer:micrometer-registry-prometheus:1.13.0")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
--- src/main/java/com/artemistechnica/federation/controllers/SimpleController.java
+++ src/main/java/com/artemistechnica/federation/controllers/SimpleController.java
@@ -1,5 +1,6 @@
 package com.artemistechnica.federation.controllers;
 
+import com.artemistechnica.commons.datatypes.CompletableFutureE;
 import com.artemistechnica.commons.datatypes.EitherE;
 import com.artemistechnica.commons.datatypes.Envelope;
 import com.artemistechnica.commons.datatypes.Pair;
@@ -10,7 +11,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.IntStream;
 
 @RestController
 public class SimpleController implements Federation {
@@ -45,12 +52,64 @@ public class SimpleController implements Federation {
                 // Materialize the result from the [[CompletableFutureE]]
                 .materialize()
                 // Materialize an [[Envelope]] from an [[EitherE]]
-                .materialize(
+                .resolve(
                         // Handle the error
                         err     -> Envelope.mkFailure(err.error),
                         // Handle the success
                         model   -> Envelope.mkSuccess(model)
                 );
+    }
+
+    @GetMapping("/envelope/async/advanced")
+    public @ResponseBody Envelope<SampleModel> getSampleEnvelopeAsyncMany() throws InterruptedException {
+
+        // TODO Major refactor. Look at condensing and adding to commons-java (i.e. map reduce)
+
+        long                                            start               = System.currentTimeMillis();
+        List<Integer>                                   workerCount         = IntStream.rangeClosed(1, 10).boxed().toList();
+        AtomicReference<List<SampleModel>>              results             = new AtomicReference<>(new ArrayList<>());
+        AtomicReference<Boolean>                        masterComplete      = new AtomicReference<>(false);
+        List<EitherE<CompletableFutureE<SampleModel>>>  threads             = workerCount
+                .stream()
+                .map(i -> EitherE.success(i).mapAsyncE(id -> {
+                    AtomicReference<Boolean> complete = new AtomicReference<>(false);
+                    CompletableFutureE<String> worker = EitherE.success(id).mapAsyncE(workId -> {
+                        long sleepLength = ThreadLocalRandom.current().nextLong(2500, 8000 + 1);
+                        try {
+                            Thread.sleep(sleepLength);
+                            complete.set(true);
+                            return String.format("Work ID: %d, Thread ID: %s Thread sleep: %d", Thread.currentThread().threadId(), id, sleepLength);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    });
+
+                    return worker.mapAsyncE(msg -> {
+                        while (!complete.get()) {
+                            try {
+                                Thread.sleep(100);
+                            } catch (InterruptedException e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+                        SampleModel model = SampleModel.mk("meta", msg);
+                        results.updateAndGet(l -> { l.add(model); return l; });
+                        return model; }
+                    );
+                }).materialize(2000, TimeUnit.MILLISECONDS)).toList();
+
+        // Blocking
+        while (!masterComplete.get()) {
+            Thread.sleep(250);
+            masterComplete.updateAndGet(c -> threads.stream().filter(workerThread -> workerThread
+                .map(CompletableFutureE::isDone)
+                .resolve(err -> false, HelperFunctions::identity)).toList().size() == threads.size());
+        }
+
+        long end = System.currentTimeMillis();
+        return Envelope.mkSuccess(
+                results.updateAndGet(r -> { r.addFirst(SampleModel.mk("total", Long.toString(end - start))); return r; }).toArray(new SampleModel[0])
+        );
     }
 
     @GetMapping("/envelope/async/error")
@@ -60,7 +119,7 @@ public class SimpleController implements Federation {
                 // Need to explicitly parameterize with <SampleModel> since we're purposefully throwing an exception
                 .<SampleModel>mapAsyncE(model -> { throw new RuntimeException(String.format("Error raised processing model with values: %s and %s", model.valueA, model.valueB)); })
                 .materialize()
-                .materialize(
+                .resolve(
                         // Handle the error
                         err     -> Envelope.mkFailure(err.error),
                         // Handle the success
@@ -81,7 +140,7 @@ public class SimpleController implements Federation {
         return federate(ctx ->  { throw new RuntimeException("Exception raised!"); })
                 .apply(Context.mk(UUID.randomUUID().toString()))
                 .flatMapE(mat -> mat.materialize(c -> SampleModel.mk("SUCCESS", c.value)))
-                .materialize(
+                .resolve(
                         error -> Envelope.mkFailure(String.format("Materialized error: %s", error.error)),
                         model -> Envelope.mkSuccess(model)
                 );

91ba92033d32659adcf29f429b933b6403607da8
nickchampagne <nick.champagne@protonmail.com>
2024-06-17 18:47:17 -0400

    Spring boot version bump to 3.3.0. Prometheus dep version bump to 1.13.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
 	java
-	id("org.springframework.boot") version "3.2.5"
+	id("org.springframework.boot") version "3.3.0"
 	id("io.spring.dependency-management") version "1.1.4"
 }
 
@@ -33,9 +33,8 @@ dependencies {
 	annotationProcessor("org.projectlombok:lombok")
 	implementation("io.github.git-commit-id:git-commit-id-maven-plugin:8.0.2")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("org.springframework.boot:spring-boot-starter-web:3.2.5")
 	implementation("com.artemistechnica.commons:commons-java:0.0.6-SNAPSHOT")
-	implementation("io.micrometer:micrometer-registry-prometheus:1.13.0")
+	implementation("io.micrometer:micrometer-registry-prometheus:1.13.1")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	implementation("org.webjars:bootstrap:5.3.3")

3d22d33967a434389323ae16f8d733a03c0157ae
nickchampagne <nick.champagne@protonmail.com>
2024-06-30 14:31:45 -0400

    Initial commit - codegen and swagger docs

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.3.0"
 	id("io.spring.dependency-management") version "1.1.4"
+	id("org.hidetake.swagger.generator") version "2.19.2"
 }
 
 group = "com.artemistechnica.federation"
@@ -31,6 +32,7 @@ repositories {
 dependencies {
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
 	implementation("io.github.git-commit-id:git-commit-id-maven-plugin:8.0.2")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("com.artemistechnica.commons:commons-java:0.0.6-SNAPSHOT")
@@ -42,10 +44,27 @@ dependencies {
 	implementation("com.github.loki4j:loki-logback-appender:1.5.1")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	swaggerCodegen("io.swagger:swagger-codegen-cli:2.4.34")
+	swaggerCodegen("io.swagger.codegen.v3:swagger-codegen-cli:3.0.47")
+	swaggerCodegen("org.openapitools:openapi-generator-cli:3.3.4")
+	swaggerUI("org.webjars:swagger-ui:4.1.3-1")
+}
+
+tasks.processResources {
+	from("api").into("api")
 }
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+swaggerSources {
+//	template {
+//		inputFile = file("template.yaml")
+//		code {
+//			language = "spring"
+//		}
+//	}
 }
 
 //tasks.jar {
